a1bda34bc05cb67afeaf9155389923169159a3bc
Wonjin <cwonjin1@gmail.com>
2020-12-23 12:58:26 -0500

    commit tree ed232d55ca8bf0ba90529b2f99890ae66b62cccf

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,30 @@
+require 'pry'
+class Patient
+    attr_accessor :name, :appointment, :doctor
+@@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+        #binding.pry
+    end
+    
+    def appointments
+        Appointment.all.select {|appointment| appointment.patient == self}
+      end
+ 
+    def doctors
+        appointments.map {|appointment| appointment.doctor}
+        #binding.pry
+    end
+    
+
+end
